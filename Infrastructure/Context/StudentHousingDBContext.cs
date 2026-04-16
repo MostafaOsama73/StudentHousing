2a39dc90d73dc326f534caf1dc89690b654a1909
Mostafa Osama <mustafa.osama@ai.menofia.edu.eg>
2026-04-16 15:21:09 +0200

    Add project files.

---
+++ Infrastructure/Context/StudentHousingDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context;
+
+public class StudentHousingDBContext
+{
+}
---
+++ Infrastructure/Entities/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Entities;
+
+public class Student
+{
+    public Guid StudentId { get; set; }
+    public string? UserId { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Gender { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string PreferredArea { get; set; }
+    public string NationalId { get; set; }
+ 
+    public virtual User? User { get; set; }
+
+}
